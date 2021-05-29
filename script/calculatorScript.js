92b131f80557cbd7839a8a39782c1bab8664bfa9
Hatsumi07 <crystalhatsumi07@gmail.com>
2021-05-28 23:45:10 -0500

    commit

---
+++ script/calculatorScript.js
@@ -0,0 +1,32 @@
+function calculation() {
+    //Asks for numbers and the operator
+    let firstNumber = parseFloat(document.getElementById("input1").value);
+    let operator = document.getElementById("operator").value;
+    let secondNumber = parseFloat(document.getElementById("input2").value);
+    let answer = parseFloat(document.getElementById("answer").value);
+    let message = "Incorrect 😢";
+    let answerKey = 0
+    document.getElementById("output").innerHTML = message;
+    switch (operator) { //verifies if the operator is +, -, *, /
+        case "+":
+            answerKey = firstNumber + secondNumber;  
+            break;  
+        case "-":
+            answerKey = firstNumber - secondNumber;  
+            break;  
+        case "*":
+            answerKey = firstNumber * secondNumber;
+            break;  
+        case "/":
+            answerKey = firstNumber / secondNumber;
+            break;  
+        default:
+            message = "You must enter a valid operator";
+
+            }
+    if (answer == answerKey) {
+        message = "Correct! 🏆";
+    
+    }    
+    document.getElementById("output").innerHTML = message;
+}  
